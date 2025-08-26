e0a6709ccebc95ac7837a8a1c962f7a987cae534
David-ZemaP <david@David.>
2025-08-25 23:07:43 -0400

    feat: Saludar por genero
    No esta muy elaborado porque el saludo no varia mucho dependiendo del
    genero

--- src/presenter.js
+++ src/presenter.js
@@ -1,6 +1,8 @@
 const nombre = document.querySelector('#nombre');
+const genero = document.querySelector('#genero');
 const enviar = document.querySelector('#enviar');
 const div = document.querySelector('#respuesta-div');
+
 
 const getGreeting = (hour) => {
   const result = 'Buenas noches';
@@ -12,10 +14,19 @@ const getGreeting = (hour) => {
   return result;
 };
 
+const adjustGreetingForGender = (greeting, gender) => {
+  if (gender === 'femenino') {
+    return greeting.replace('Buenos', 'Buenas');
+  }
+  return greeting;
+};
+
 enviar.addEventListener('click', (event) => {
   event.preventDefault();
 
   const hour = new Date().getHours();
+  const greeting = getGreeting(hour);
+  const adjustedGreeting = adjustGreetingForGender(greeting, genero.value);
 
-  div.innerHTML = `<p>${getGreeting(hour)} ${nombre.value}</p>`;
+  div.innerHTML = `<p>${adjustedGreeting} ${nombre.value}</p>`;
 });
