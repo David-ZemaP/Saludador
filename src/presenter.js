169010d6d5a846344ce0dc3219dafee3a6476e15
David-ZemaP <david@David.>
2025-08-25 22:52:26 -0400

    feat: Saludar segun la hora

--- src/presenter.js
+++ src/presenter.js
@@ -2,8 +2,20 @@ const nombre = document.querySelector('#nombre');
 const enviar = document.querySelector('#enviar');
 const div = document.querySelector('#respuesta-div');
 
+const getGreeting = (hour) => {
+  const result = 'Buenas noches';
+  if (hour >= 5 && hour < 12) {
+    result = 'Buenos días';
+  } if (hour >= 12 && hour < 18) {
+    result = 'Buenas tardes';
+  }
+  return result;
+};
+
 enviar.addEventListener('click', (event) => {
   event.preventDefault();
-  
-  div.innerHTML = '<p>Hola ' + nombre.value + '</p>';
+
+  const hour = new Date().getHours();
+
+  div.innerHTML = `<p>${getGreeting(hour)} ${nombre.value}</p>`;
 });
