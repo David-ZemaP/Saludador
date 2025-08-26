b7390bdd2e988c7a113c32e89b1fd66ac6457122
David-ZemaP <david@David.>
2025-08-25 23:58:00 -0400

    fix: Errors in npm start

--- src/presenter.js
+++ src/presenter.js
@@ -1,31 +1,11 @@
+import { getGreeting, adjustGreetingForGenderAge } from './saludador.js';
+
 const nombre = document.querySelector('#nombre');
 const genero = document.querySelector('#genero');
 const enviar = document.querySelector('#enviar');
 const div = document.querySelector('#respuesta-div');
 const edad = document.querySelector('#edad');
 
-
-const getGreeting = (hour) => {
-  const result = 'Buenas noches';
-  if (hour >= 5 && hour < 12) {
-    result = 'Buenos días';
-  } if (hour >= 12 && hour < 18) {
-    result = 'Buenas tardes';
-  }
-  return result;
-};
-
-const adjustGreetingForGenderAge = (greeting, gender, age) => {
-  // Then, add formal titles for people over 30
-  if (gender === 'femenino' && age > 30) {
-    return `${greeting} Sra.`;
-  }
-  if (gender === 'masculino' && age > 30) {
-    return `${greeting} Sr.`;
-  }
-
-  return greeting;
-};
 
 enviar.addEventListener('click', (event) => {
   event.preventDefault();
--- src/saludador.js
+++ src/saludador.js
@@ -1,8 +1,21 @@
-const nombre = document.getElementById("#nombre");
-const form = document.querySelector("#saludar-form");
+export function getGreeting(hour){
+  let result = 'Buenas noches';
+  if (hour >= 5 && hour < 12) {
+    result = 'Buenos días';
+  } else if (hour >= 12 && hour < 18) {
+    result = 'Buenas tardes';
+  }
+  return result;
+}
 
-form.addEventListener("click", (event) => {
-  event.preventDefault();
+export function adjustGreetingForGenderAge(greeting, gender, age){
+  // Then, add formal titles for people over 30
+  if (gender === 'femenino' && age > 30) {
+    return `${greeting} Sra.`;
+  }
+  if (gender === 'masculino' && age > 30) {
+    return `${greeting} Sr.`;
+  }
 
-  div.innerHTML = "<p> Hola " + nombre + "</p>";
-});
+  return greeting;
+}
